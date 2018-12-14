9d9fcbfb0b56a8cbaca8be06f3a6e52ad9903fa4
Britt Carter <brittbmh@gmail.com>
2018-12-14 15:52:37 -0600

    created basics on the DOM

--- client.js
+++ client.js
@@ -1,1 +1,12 @@
 console.log('js');
+
+$(document).ready(function () {
+    $('#submit').on('click', addEmployee);
+
+});
+
+function addEmployee() {
+    let firstName = $('#firstNameEE').val();
+    console.log(firstName);
+     
+}
